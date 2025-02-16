51fc1996f9dccef52487825942d7210ba933a987
EveryPine <starcraft819@gmail.com>
2025-02-16 22:03:47 +0900

    initial commit

---
+++ src/main/java/com/groommoa/aether_back_apigateway/AetherBackApigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.groommoa.aether_back_apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AetherBackApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AetherBackApigatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groommoa/aether_back_apigateway/AetherBackApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groommoa.aether_back_apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AetherBackApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
