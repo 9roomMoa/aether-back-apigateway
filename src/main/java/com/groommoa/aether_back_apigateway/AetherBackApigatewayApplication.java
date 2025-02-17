7e0ad99508e2652b53cbd68edaf471185a16e9bc
EveryPine <starcraft819@gmail.com>
2025-02-17 23:54:08 +0900

    chore: 빌드 오류 수정

--- src/main/java/com/groommoa/aether_back_apigateway/AetherBackApigatewayApplication.java
+++ src/main/java/com/groommoa/aether_back_apigateway/AetherBackApigatewayApplication.java
@@ -2,8 +2,10 @@ package com.groommoa.aether_back_apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class AetherBackApigatewayApplication {
 
 	public static void main(String[] args) {
