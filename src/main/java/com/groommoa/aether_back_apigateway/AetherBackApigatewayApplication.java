b3506fd7978e1cf92451b923ed68fbdea13aaec0
EveryPine <starcraft819@gmail.com>
2025-02-20 15:13:38 +0900

    build: eureka client 등록
    - 서버 포트: 8081

--- src/main/java/com/groommoa/aether_back_apigateway/AetherBackApigatewayApplication.java
+++ src/main/java/com/groommoa/aether_back_apigateway/AetherBackApigatewayApplication.java
@@ -2,7 +2,6 @@ package com.groommoa.aether_back_apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 public class AetherBackApigatewayApplication {
